07d74707b2769254f91ddbc2fe5f0e958f9a669d
mark valdez <jhonmarkprudentevaldezcoder@gmail.com>
2023-10-09 20:47:29 +0800

    dada

--- server.js
+++ server.js
@@ -13,10 +13,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cors({ origin: "http://localhost:3000" }));
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  const allowedOrigins = [
+    "http://localhost:3000",
+    "https://kvnhswebsite.vercel.app",
+  ];
 
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
   next();
 });
 
