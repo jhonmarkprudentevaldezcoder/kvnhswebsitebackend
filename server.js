1e1e1421dbd3e2a324615f1a5f7d39a1420ae4fc
mark valdez <jhonmarkprudentevaldezcoder@gmail.com>
2023-10-09 16:45:58 +0800

    update

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 const express = require("express");
+const cors = require("cors");
+const httpProxy = require("http-proxy");
 const mongoose = require("mongoose");
 const Student = require("./models/studentModel");
-
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
-
 const app = express();
+const proxy = httpProxy.createProxyServer();
 
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors({ origin: "http://localhost:3000" }));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 
 //default route
 app.get("/", (req, res) => {
